36aa44853402e0f41ee84ea1970d21aba4ad89fe
Raphael Martins <rapha@raphael-martins.com>
2026-04-21 20:40:22 -0300

    confiar no proxy do aaPanel

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*'); // confiar no proxy do aaPanel
         $middleware->validateCsrfTokens(except: [
             'webhook/whatsapp',
         ]);
