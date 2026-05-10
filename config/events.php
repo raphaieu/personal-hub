1b752eb5df68b53d906f5c203340fa3b06463c03
Raphael Martins <rapha@raphael-martins.com>
2026-05-10 19:57:52 -0300

    feat: Configura CORS, Turnstile e Rate Limiting para Eventos

---
+++ config/events.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+
+    'frontend_url' => rtrim((string) env('EVENTS_FRONTEND_URL', 'https://events.raphael-martins.com'), '/'),
+
+    'default_terms_url' => env('EVENTS_DEFAULT_TERMS_URL', 'https://events.raphael-martins.com/termos'),
+
+    'default_privacy_url' => env('EVENTS_DEFAULT_PRIVACY_URL', 'https://events.raphael-martins.com/privacidade'),
+
+    'turnstile' => [
+        'enabled' => filter_var(env('EVENTS_TURNSTILE_ENABLED', true), FILTER_VALIDATE_BOOL),
+        'secret_key' => env('EVENTS_TURNSTILE_SECRET_KEY'),
+    ],
+];
